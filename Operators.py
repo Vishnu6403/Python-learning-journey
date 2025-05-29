0d8ba398818e65e0e5de5e94bc8c15c78c783e52
Vishnu <123068405+Vishnu6403@users.noreply.github.com>
2025-05-29 17:57:32 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,56 @@
+#operators
+''' symbols which perform actions
+
+1.Arithematic operators
+   arithematic
+   +
+   -
+   * multiplication
+   /  division
+   ** power  a**2 a**10
+   // floor division
+   % modulo reminder
+   '''
+
+a=int(input("Enter a value for a"))
+b=int(input("Enter a value for b"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+print(a%b)
+
+
+''' Relational operators
+
+>,<,>=,<=,==,!=
+a==5  a!=5'''
+print(a==b)
+print(a<=b)
+
+''' Assignment operators
+=
+a=5
+a=a+2   a+=2,-=,*='''
+print(a)
+a+=4
+print(a)
+a-=2
+print(a)
+a*=5
+print(a)
+a/=3
+print(a)
+
+''' Logical operators
+c1 and c2   total>80 and maths>80
+c1 or  c2   total>80 or maths>90
+c1        '''
+a=int(input("a"))
+b=int(input("b"))
+
+print(a>4 and b>2)
+print(a>4 or b>2)
+
